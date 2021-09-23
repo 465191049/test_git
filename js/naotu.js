b5d342010cefa29d8ec637d95ae996aa9d145de0
465191049 <465191049@qq.com>
2021-09-23 20:56:18 +0800

    Create naotu.js

---
+++ js/naotu.js
@@ -0,0 +1,21 @@
+var nt={};
+nt.h_interval=50;
+nt.v_interval=10;
+
+nt.test=function(){
+    var svg_1='<g id="kity_g_8" transform="translate(0.5, 0.5)">'
+				+	'<g id="minder1" text-rendering="optimize-speed" transform="translate( 373 335 )">'
+				+		'<g id="minder_node1" transform="matrix( 1 0 0 1 0 0 )">'
+				+			'<path id="node_outline1" fill="rgb(115, 161, 191)" stroke="rgb(57, 80, 96)" d="M-19,-20h96a5,5,0,0,1,5,5v30a5,5,0,0,1,-5,5h-96a5,5,0,0,1,-5,-5v-30a5,5,0,0,1,5,-5z" stroke-width="3">'
+				+			'</path>'
+				+			'<g id="node_text1" transform="translate( 0 -2.4 )" fill="white">'
+				+				'<text id="kity_text_22" text-rendering="inherit" dominant-baseline="text-before-edge" font-size="16" dy="0" y="-8">aaa'
+				+				'</text>'
+				+			'</g>'
+				+		'</g>'
+				+	'</g>'
+				+'</g>' ;
+				
+	$('#svg_1').html(svg_1);
+
+};
